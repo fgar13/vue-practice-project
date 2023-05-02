ee3fb29a36f63b875146d39ed3e8c65d3f0ca38f
fgar13 <94147771+fgar13@users.noreply.github.com>
2023-05-02 00:19:53 -0400

    Created student table

--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -1,5 +1,50 @@
 <template>
-  <main>
-    <h1>Student page</h1>
-  </main>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          Students
+          <RouterLink to="/student/create" class="btn btn-primary float-end">
+            Add Student
+          </RouterLink>
+        </h4>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered">
+          <thead> 
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Course</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Created At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+
+export default{
+  name: 'students',
+  data(){
+    return{
+      students: []
+    }
+  },
+  mounted(){
+    console.log('I am here')
+  }
+}
+
+</script>
