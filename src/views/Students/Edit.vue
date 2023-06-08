5bb6c8f7b75295f0e4cb460f351278fa50c3eac7
fgar13 <94147771+fgar13@users.noreply.github.com>
2023-06-08 18:53:30 -0400

    Crud functionality complete

--- src/views/Students/Edit.vue
+++ src/views/Students/Edit.vue
@@ -2,32 +2,15 @@
     <div class="container mt-5">
         <div class="card">
             <div class="card-header">
-                <h4>Add Students</h4>
+                <h4>Edit Students</h4>
             </div>
             <div class="card-body">
-                <!-- <ul class="alert alert-warning" v-if="Object.keys(this.errorList).length > 0">
-                    <li class="mb-0 ms-3" v-for="(error, index) in this.errorList" :key="index">
-                        {{error[0]}}
-                    </li>
-                </ul>
-                <div class="mb-3">
-                    <label for="">userID</label>
-                    <input type="text" v-model="model.student.userId" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">title</label>
-                    <input type="text" v-model="model.student.title" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="">body</label>
-                    <input type="text" v-model="model.student.body" class="form-control">
-                </div> -->
                 <div class="mb-3">
                     <label for="">Name</label>
                     <input type="text" v-model="model.student.name" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <button type="button" @click="saveStudent" class="btn btn-primary">Save</button>
+                    <button type="button" @click="updateStudent" class="btn btn-primary">Update</button>
                 </div>
             </div>
         </div>
@@ -37,9 +20,10 @@
 <script>
 import axios from 'axios'
 export default {
-    name: 'studentCreate',
+    name: 'studentEdit',
     data(){
         return {
+            studentId: '',
             errorList: '',
             model: {
                 student: {
@@ -53,22 +37,44 @@ export default {
     },
     mounted(){
         console.log(this.$route.params.id);
+        this.studentId = this.$route.params.id;
         this.getStudentData(this.$route.params.id)
     },
     methods: {
-        saveStudent(){
+        getStudentData(studentId){
+            axios.get(`http://127.0.0.1:8000/edit/${studentId}/`).then(res => {
+                console.log(res.data)
+                alert("alert")
+                this.model.student.name = res.data.student.name
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    if(error.response.status == 404){
+                        mythis.errorList = error.response//.data;
+                    }
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+                } else if (error.request) {
+                console.log(error.request);
+                } else {
+                console.log('Error', error.message);
+                }
+            });
+         },
+        updateStudent(){
             var mythis = this;
             //http://127.0.0.1:8000/add
             //https://jsonplaceholder.typicode.com/posts
-            axios.post('http://127.0.0.1:8000/add/', this.model.student).then(res => {
+            axios.put(`http://127.0.0.1:8000/edit/${this.studentId}/`, this.model.student).then(res => {
                 console.log(res.data)
                 alert(res.data.message);
-                this.model.student = {
-                    // userId: '',
-                    // title: '',
-                    // body: ''
-                    name: ''
-                }
+                // this.model.student = {
+                //     // userId: '',
+                //     // title: '',
+                //     // body: ''
+                //     name: ''
+                // }
             })
             .catch(function (error) {
                 if (error.response) {
--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -4,7 +4,7 @@
       <div class="card-header">
         <h4>
           Students
-          <RouterLink to="/student/create" class="btn btn-primary float-end">
+          <RouterLink to="/students/create" class="btn btn-primary float-end">
             Add Student
           </RouterLink>
         </h4>
@@ -13,23 +13,21 @@
         <table class="table table-bordered">
           <thead> 
             <tr>
-              <th>UserID</th>
               <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
+              <th>Name</th>
+              <th>Created</th>
             </tr>
           </thead>
           <tbody v-if="this.students.length>0">
             <tr v-for="(student, index) in students" :key="index">
-              <td>{{ student.userId }}</td>
               <td>{{ student.id }}</td>
-              <td>{{ student.title }}</td>
-              <td>{{ student.body }}</td>
+              <td>{{ student.name }}</td>
+              <td>{{ student.created }}</td>
               <td>
                 <RouterLink :to="{path: '/students/'+student.id+'/edit'}" class="btn btn-success mx-2">
                   Edit
                 </RouterLink>
-                <button type="button" class="btn btn-danger mx-2">
+                <button type="button" @click="deleteStudent(student.id)" class="btn btn-danger mx-2">
                   Delete
                 </button>
               </td>
@@ -63,10 +61,18 @@ export default {
   methods: {
     //https://jsonplaceholder.typicode.com/posts http://127.0.0.1:8000/
     getStudents() {
-      axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      axios.get('http://127.0.0.1:8000/').then((res) => {
         this.students = res.data//.students
         console.log(this.students)
       });
+    },
+    deleteStudent(studentId){
+      console.log(studentId)
+      if(confirm("U sure?")){
+        axios.delete(`http://127.0.0.1:8000/delete/${studentId}/`).then(res => {
+          alert("done")
+        });
+      }
     },
   },
 };
